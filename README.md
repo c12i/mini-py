043f06f8e1b0f4f8d7cd672c4e68f3fc6c352b55
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-04-13 15:23:19 +0300

    extend api

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 An **extremely** simplified way to run python in the browser with a similar API to the [`<py-script></py-script>`](https://pyscript.net) tag, re-implemented as `<mini-py></mini-py>`, using [web-components](https://developer.mozilla.org/en-US/docs/Web/API/Web_components) and [pyodide](https://pyodide.org)
 
 
+## Examples
+
+- default behaviour is to run the python code in the `mini-py` element
+
 ```html
 <mini-py>
 import js
@@ -11,4 +15,40 @@ div = js.document.createElement("h1")
 div.innerHTML = "This element was created from Python"
 js.document.body.prepend(div)
 </mini-py>
+```
+
+- with `autoRun` set to `"false"`
+
+```html
+<mini-py autorun="false">print("Hello, World!")</mini-py>
+<button onclick="document.querySelector('mini-py').runCode()">Run Code</button>
+```
+
+- event handling
+
+```html
+<mini-py id="python-script">print("Hello, World!")</mini-py>
+<script>
+  const pythonElement = document.getElementById("python-script");
+
+  pythonElement.addEventListener("pyodide-loading", () => {
+    console.log("Pyodide is loading...");
+  });
+
+  pythonElement.addEventListener("pyodide-loaded", () => {
+    console.log("Pyodide has loaded.");
+  });
+
+  pythonElement.addEventListener("code-execution-started", () => {
+    console.log("Python code execution has started.");
+  });
+
+  pythonElement.addEventListener("code-execution-completed", (event) => {
+    console.log("Python code execution completed. Result:", event.detail.result);
+  });
+
+  pythonElement.addEventListener("code-execution-error", (event) => {
+    console.error("Python code execution error:", event.detail.error);
+  });
+</script>
 ```
--- index.js
+++ index.js
@@ -1,28 +1,61 @@
 class MiniPy extends HTMLElement {
   constructor() {
     super();
-    this.code = ''
+    this.code = "";
+    this.autoRun = true;
   }
 
   connectedCallback() {
     this.code = this.textContent.trim();
-    this.textContent = '';
-    this.initializePyodideAndRunCode();
+    this.textContent = "";
+    this.autoRun = this.hasAttribute("autorun") ? (this.getAttribute("autorun") === "true") : true;
+    if (this.autoRun) {
+      this._initializePyodideAndRunCode();
+    }
   }
 
-  initializePyodideAndRunCode() {
+  disconnectedCallback() {
+    if (this.abortController) {
+      this.abortController.abort();
+    }
+  }
+
+  runCode() {
+    this._initializePyodideAndRunCode();
+  }
+
+
+  _initializePyodideAndRunCode() {
+    this._dispatchEvent("pyodide-loading");
     if (!window.pyodidePromise) {
       window.pyodidePromise = loadPyodide();
+      window.pyodidePromise.then(() => {
+        this._dispatchEvent('pyodide-loaded')
+      });
     }
     window.pyodidePromise
       .then(pyodide => {
+        this._dispatchEvent("code-execution-started");
         return pyodide.runPython(this.code);
       })
-      .then(console.log)
+      .then(result => {
+        this._dispatchEvent("code-execution-completed");
+        console.log(result);
+      })
       .catch(error => {
-        console.error('Python execution error:', error);
+        this._dispatchEvent('code-execution-error', {error: error});
+        console.error("Python execution error:", error);
       });
+  }
+
+  _dispatchEvent(name, detail = {}) {
+    const event = new CustomEvent(name, {
+      bubbles: true,
+      composed: true,
+      detail
+    })
+    this.dispatchEvent(event)
   }
 }
 
-customElements.define('mini-py', MiniPy);
+customElements.define("mini-py", MiniPy);
